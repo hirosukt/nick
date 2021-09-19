71ad76306420b9f658946b56553a57c652264fec
Hirosuke <senolynn7584@gmail.com>
2021-09-19 15:16:47 +0900

    game profile builder convert to kotlin

--- src/main/java/hirosuke/nick/GameProfileBuilder.kt
+++ src/main/java/hirosuke/nick/GameProfileBuilder.kt
@@ -1,53 +1,40 @@
-package hirosuke.nick;
+package hirosuke.nick
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.UUID;
+import com.google.gson.*
+import java.util.UUID
+import com.mojang.util.UUIDTypeAdapter
+import com.mojang.authlib.GameProfile
+import com.mojang.authlib.properties.Property
+import com.mojang.authlib.properties.PropertyMap
+import hirosuke.nick.GameProfileBuilder
+import java.util.HashMap
+import hirosuke.nick.GameProfileBuilder.CachedProfile
+import org.apache.commons.io.IOUtils
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
+import kotlin.jvm.JvmOverloads
+import kotlin.Throws
+import java.io.IOException
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.reflect.Type
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.util.ArrayList
 
-import com.google.gson.*;
-import org.apache.commons.io.IOUtils;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
-
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
-import com.mojang.util.UUIDTypeAdapter;
-
-public class GameProfileBuilder {
-
-    private static final String SERVICE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/%s?unsigned=false";
-    private static final String JSON_SKIN = "{\"timestamp\":%d,\"profileId\":\"%s\",\"profileName\":\"%s\",\"isPublic\":true,\"textures\":{\"SKIN\":{\"url\":\"%s\"}}}";
-    private static final String JSON_CAPE = "{\"timestamp\":%d,\"profileId\":\"%s\",\"profileName\":\"%s\",\"isPublic\":true,\"textures\":{\"SKIN\":{\"url\":\"%s\"},\"CAPE\":{\"url\":\"%s\"}}}";
-
-    private static Gson gson = new GsonBuilder().disableHtmlEscaping().registerTypeAdapter(UUID.class, new UUIDTypeAdapter()).registerTypeAdapter(GameProfile.class, new GameProfileSerializer()).registerTypeAdapter(PropertyMap.class, new PropertyMap.Serializer()).create();
-
-    private static HashMap<UUID, CachedProfile> cache = new HashMap<UUID, CachedProfile>();
-
-    private static long cacheTime = -1;
-
-    /**
-     * Don't run in main thread!
-     *
-     * Fetches the GameProfile from the Mojang servers
-     *
-     * @param uuid The player uuid
-     * @return The GameProfile
-     * @throws IOException If something wents wrong while fetching
-     * @see GameProfile
-     */
-    public static GameProfile fetch(UUID uuid) throws IOException {
-        return fetch(uuid, false);
-    }
-
+object GameProfileBuilder {
+    private const val SERVICE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/%s?unsigned=false"
+    private const val JSON_SKIN =
+        "{\"timestamp\":%d,\"profileId\":\"%s\",\"profileName\":\"%s\",\"isPublic\":true,\"textures\":{\"SKIN\":{\"url\":\"%s\"}}}"
+    private const val JSON_CAPE =
+        "{\"timestamp\":%d,\"profileId\":\"%s\",\"profileName\":\"%s\",\"isPublic\":true,\"textures\":{\"SKIN\":{\"url\":\"%s\"},\"CAPE\":{\"url\":\"%s\"}}}"
+    private val gson = GsonBuilder().disableHtmlEscaping().registerTypeAdapter(
+        UUID::class.java, UUIDTypeAdapter()
+    ).registerTypeAdapter(GameProfile::class.java, GameProfileSerializer()).registerTypeAdapter(
+        PropertyMap::class.java, PropertyMap.Serializer()
+    ).create()
+    private val cache = HashMap<UUID, CachedProfile>()
+    private var cacheTime: Long = -1
     /**
      * Don't run in main thread!
      *
@@ -58,25 +45,39 @@ public class GameProfileBuilder {
      * @throws IOException If something wents wrong while fetching
      * @see GameProfile
      */
-    public static GameProfile fetch(UUID uuid, boolean forceNew) throws IOException {
-        if (!forceNew && cache.containsKey(uuid) && cache.get(uuid).isValid()) {
-            return cache.get(uuid).profile;
+    /**
+     * Don't run in main thread!
+     *
+     * Fetches the GameProfile from the Mojang servers
+     *
+     * @param uuid The player uuid
+     * @return The GameProfile
+     * @throws IOException If something wents wrong while fetching
+     * @see GameProfile
+     */
+    @JvmOverloads
+    @Throws(IOException::class)
+    fun fetch(uuid: UUID, forceNew: Boolean = false): GameProfile {
+        return if (!forceNew && cache.containsKey(uuid) && cache[uuid]!!.isValid) {
+            cache[uuid]!!.profile
         } else {
-            HttpURLConnection connection = (HttpURLConnection) new URL(String.format(SERVICE_URL, UUIDTypeAdapter.fromUUID(uuid))).openConnection();
-            connection.setReadTimeout(5000);
-
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                String json = IOUtils.toString( connection.getInputStream(), StandardCharsets.UTF_8 );
-
-                GameProfile result = gson.fromJson(json, GameProfile.class);
-                cache.put(uuid, new CachedProfile(result));
-                return result;
+            val connection =
+                URL(String.format(SERVICE_URL, UUIDTypeAdapter.fromUUID(uuid)))
+                    .openConnection() as HttpURLConnection
+            connection.readTimeout = 5000
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val json =
+                    IOUtils.toString(connection.inputStream, StandardCharsets.UTF_8)
+                val result = gson.fromJson(json, GameProfile::class.java)
+                cache[uuid] = CachedProfile(result)
+                result
             } else {
                 if (!forceNew && cache.containsKey(uuid)) {
-                    return cache.get(uuid).profile;
+                    return cache[uuid]!!.profile
                 }
-                JsonObject error = (JsonObject) new JsonParser().parse(new BufferedReader(new InputStreamReader(connection.getErrorStream())).readLine());
-                throw new IOException(error.get("error").getAsString() + ": " + error.get("errorMessage").getAsString());
+                val error = JsonParser()
+                    .parse(BufferedReader(InputStreamReader(connection.errorStream)).readLine()) as JsonObject
+                throw IOException(error["error"].asString + ": " + error["errorMessage"].asString)
             }
         }
     }
@@ -90,8 +91,8 @@ public class GameProfileBuilder {
      * @return A GameProfile built from the arguments
      * @see GameProfile
      */
-    public static GameProfile getProfile(UUID uuid, String name, String skin) {
-        return getProfile(uuid, name, skin, null);
+    fun getProfile(uuid: UUID?, name: String?, skin: String?): GameProfile {
+        return getProfile(uuid, name, skin, null)
     }
 
     /**
@@ -104,69 +105,64 @@ public class GameProfileBuilder {
      * @return A GameProfile built from the arguments
      * @see GameProfile
      */
-    public static GameProfile getProfile(UUID uuid, String name, String skinUrl, String capeUrl) {
-        GameProfile profile = new GameProfile(uuid, name);
-        boolean cape = capeUrl != null && !capeUrl.isEmpty();
-
-        List<Object> args = new ArrayList<Object>();
-        args.add(System.currentTimeMillis());
-        args.add(UUIDTypeAdapter.fromUUID(uuid));
-        args.add(name);
-        args.add(skinUrl);
-        if (cape) args.add(capeUrl);
-
-        profile.getProperties().put("textures", new Property("textures", Base64Coder.encodeString(String.format(cape ? JSON_CAPE : JSON_SKIN, args.toArray(new Object[args.size()])))));
-        return profile;
+    fun getProfile(uuid: UUID?, name: String?, skinUrl: String?, capeUrl: String?): GameProfile {
+        val profile = GameProfile(uuid, name)
+        val cape = capeUrl != null && !capeUrl.isEmpty()
+        val args: MutableList<Any?> = ArrayList()
+        args.add(System.currentTimeMillis())
+        args.add(UUIDTypeAdapter.fromUUID(uuid))
+        args.add(name)
+        args.add(skinUrl)
+        if (cape) args.add(capeUrl)
+        profile.properties.put(
+            "textures",
+            Property(
+                "textures",
+                Base64Coder.encodeString(String.format(if (cape) JSON_CAPE else JSON_SKIN, *args.toTypedArray()))
+            )
+        )
+        return profile
     }
 
     /**
      * Sets the time as long as you want to keep the gameprofiles in cache (-1 = never remove it)
      * @param time cache time (default = -1)
      */
-    public static void setCacheTime(long time) {
-        cacheTime = time;
+    fun setCacheTime(time: Long) {
+        cacheTime = time
     }
 
-    private static class GameProfileSerializer implements JsonSerializer<GameProfile>, JsonDeserializer<GameProfile> {
-
-        public GameProfile deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
-            JsonObject object = (JsonObject) json;
-            UUID id = object.has("id") ? (UUID) context.deserialize(object.get("id"), UUID.class) : null;
-            String name = object.has("name") ? object.getAsJsonPrimitive("name").getAsString() : null;
-            GameProfile profile = new GameProfile(id, name);
-
-            if (object.has("properties")) {
-                for (Entry<String, Property> prop : ((PropertyMap) context.deserialize(object.get("properties"), PropertyMap.class)).entries()) {
-                    profile.getProperties().put(prop.getKey(), prop.getValue());
+    private class GameProfileSerializer : JsonSerializer<GameProfile>, JsonDeserializer<GameProfile> {
+        @Throws(JsonParseException::class)
+        override fun deserialize(json: JsonElement, type: Type, context: JsonDeserializationContext): GameProfile {
+            val `object` = json as JsonObject
+            val id =
+                if (`object`.has("id")) context.deserialize<Any>(`object`["id"], UUID::class.java) as UUID else null
+            val name = if (`object`.has("name")) `object`.getAsJsonPrimitive("name").asString else null
+            val profile = GameProfile(id, name)
+            if (`object`.has("properties")) {
+                for ((key, value) in (context.deserialize<Any>(
+                    `object`["properties"],
+                    PropertyMap::class.java
+                ) as PropertyMap).entries()) {
+                    profile.properties.put(key, value)
                 }
             }
-            return profile;
+            return profile
         }
 
-        public JsonElement serialize(GameProfile profile, Type type, JsonSerializationContext context) {
-            JsonObject result = new JsonObject();
-            if (profile.getId() != null)
-                result.add("id", context.serialize(profile.getId()));
-            if (profile.getName() != null)
-                result.addProperty("name", profile.getName());
-            if (!profile.getProperties().isEmpty())
-                result.add("properties", context.serialize(profile.getProperties()));
-            return result;
+        override fun serialize(profile: GameProfile, type: Type, context: JsonSerializationContext): JsonElement {
+            val result = JsonObject()
+            if (profile.id != null) result.add("id", context.serialize(profile.id))
+            if (profile.name != null) result.addProperty("name", profile.name)
+            if (!profile.properties.isEmpty) result.add("properties", context.serialize(profile.properties))
+            return result
         }
-
     }
 
-    private static class CachedProfile {
-
-        private long timestamp = System.currentTimeMillis();
-        private GameProfile profile;
-
-        public CachedProfile(GameProfile profile) {
-            this.profile = profile;
-        }
-
-        public boolean isValid() {
-            return cacheTime < 0 ? true : (System.currentTimeMillis() - timestamp) < cacheTime;
-        }
+    private class CachedProfile(val profile: GameProfile) {
+        private val timestamp = System.currentTimeMillis()
+        val isValid: Boolean
+            get() = if (cacheTime < 0) true else System.currentTimeMillis() - timestamp < cacheTime
     }
 }
