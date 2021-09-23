ae1247a221ab681d072d50058733c7758bb6cf73
Hirosuke <senolynn7584@gmail.com>
2021-09-24 00:28:45 +0900

    create interface

--- src/main/java/hirosuke/nick/CommandNick.kt
+++ src/main/java/hirosuke/nick/CommandNick.kt
@@ -1,19 +1,12 @@
 package hirosuke.nick
 
-import com.mojang.authlib.GameProfile
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import java.lang.reflect.Field
-import net.minecraft.server.v1_16_R3.PacketPlayOutEntityDestroy
-import net.minecraft.server.v1_16_R3.PacketPlayOutNamedEntitySpawn
-import net.minecraft.server.v1_16_R3.PacketPlayOutPlayerInfo
-import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer
 
-class CommandNick : CommandExecutor {
-
+interface CommandNick : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender is Player) {
             if (command.name == "nick") {
@@ -46,26 +39,5 @@ class CommandNick : CommandExecutor {
         return true
     }
 
-    fun setNick(player: Player, name: String) {
-
-        player.setPlayerListName(name)
-        player.setDisplayName(name)
-        player.customName = name
-        player.isCustomNameVisible = true
-
-        for (ps in Bukkit.getOnlinePlayers()) {
-            if(ps == player) continue
-
-            (ps as CraftPlayer).handle.playerConnection.sendPacket(PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, (player as CraftPlayer).handle))
-
-            var nameField: Field = GameProfile::class.java.getDeclaredField("name")
-            nameField.isAccessible = true
-
-            nameField.set(player.profile, name)
-
-            ps.handle.playerConnection.sendPacket(PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, player.handle))
-            ps.handle.playerConnection.sendPacket(PacketPlayOutEntityDestroy(player.getEntityId()))
-            ps.handle.playerConnection.sendPacket(PacketPlayOutNamedEntitySpawn((player.handle)))
-        }
-    }
+    fun setNick(player: Player, name: String) { }
 }
